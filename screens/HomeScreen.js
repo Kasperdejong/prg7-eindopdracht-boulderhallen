dcd2e09830cdfee3444828bacd5d8aa0c747418f
Kasperdejong <kasperdejong@gmail.com>
2025-07-03 13:51:32 +0200

    Biometrics werkt niet dus alles daarvan weg.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,23 +4,18 @@ import { useNavigation } from "@react-navigation/native";
 import BoulderListItem from "../components/BoulderListItem";
 import { DarkModeContext } from "../providers/DarkModeProvider";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import ReactNativeBiometrics, { BiometryTypes } from 'react-native-biometrics'
 
 export default function HomeScreen() {
     const navigation = useNavigation();
     const { darkMode } = useContext(DarkModeContext);
     const [boulderGyms, setBoulderGyms] = useState([]);
     const [favorites, setFavorites] = useState([]);
-    const rnBiometrics = new ReactNativeBiometrics()
-
-
 
     useEffect(() => {
         const fetchBoulders = async () => {
             try {
                 const response = await fetch("https://stud.hosted.hr.nl/1078780/bouldergyms.json");
                 if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
                 const data = await response.json();
                 setBoulderGyms(data);
             } catch (err) {
@@ -34,6 +29,7 @@ export default function HomeScreen() {
                 setFavorites(JSON.parse(storedFavorites));
             }
         };
+
         fetchBoulders();
         loadFavorites();
     }, []);
@@ -43,31 +39,14 @@ export default function HomeScreen() {
     };
 
     const toggleFavorite = async (gym) => {
-        const { available, biometryType } = await rnBiometrics.isSensorAvailable();
-
-        if (!available) {
-            console.warn('Biometrics not available');
-            return;
-        }
-
-        const promptMessage = biometryType === BiometryTypes.FaceID
-            ? 'Use Face ID to authenticate'
-            : 'Authenticate to modify favorites';
-
-        const result = await rnBiometrics.simplePrompt({ promptMessage });
-
-        if (result.success) {
-            let updatedFavorites;
-            if (favorites.some(fav => fav.id === gym.id)) {
-                updatedFavorites = favorites.filter(fav => fav.id !== gym.id);
-            } else {
-                updatedFavorites = [...favorites, gym];
-            }
-            setFavorites(updatedFavorites);
-            await AsyncStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+        let updatedFavorites;
+        if (favorites.some(fav => fav.id === gym.id)) {
+            updatedFavorites = favorites.filter(fav => fav.id !== gym.id);
         } else {
-            console.log('Biometric authentication cancelled or failed');
+            updatedFavorites = [...favorites, gym];
         }
+        setFavorites(updatedFavorites);
+        await AsyncStorage.setItem('favorites', JSON.stringify(updatedFavorites));
     };
 
     const renderItem = ({ item }) => (
